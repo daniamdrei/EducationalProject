669aee337018152de0ac0452de671e6b5ba8874a
[daniamdrei] <[daniamdrei@gmail.com]>
2025-05-16 11:51:10 +0300

    crete migration files

---
+++ database/migrations/2025_05_16_082540_create_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('price', 8, 2)->default(0);
+            $table->boolean('is_free')->default(false);
+            $table->enum('level', ['beginner', 'intermediate', 'advanced'])->default('beginner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
